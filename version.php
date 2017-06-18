275067fc7c2ef8dd33c478accd7b4c02f2a833ff
valery <valery.fremaux@gmail.com>
2017-06-18 18:32:20 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015032200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016051900;        // Requires this Moodle version.
+$plugin->requires  = 2016120500;        // Requires this Moodle version.
 $plugin->component = 'block_quiz_progress'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.0.0 (Build 2015032200)';
+$plugin->release = '3.2 (Build 2015032200)';
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = array('local_vflibs' => 2015101800);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
