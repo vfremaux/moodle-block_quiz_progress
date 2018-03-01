b1438a245da0bb79bedace043b79a4add7559199
valery <valery.fremaux@gmail.com>
2018-03-01 16:34:20 +0100

    full resync

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015032200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016120500;        // Requires this Moodle version.
+$plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'block_quiz_progress'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.2 (Build 2015032200)';
-$plugin->maturity = MATURITY_RC;
+$plugin->release = '3.4.0 (Build 2015032200)';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('local_vflibs' => 2015101800);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0000';
+$plugin->codeincrement = '3.4.0000';
